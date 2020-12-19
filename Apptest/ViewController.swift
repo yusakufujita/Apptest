4c16e8682ba86b9f893f61f6c56c51534a4b013c
藤田優作 <marozaemon1217@gmail.com>
2020-12-20 02:05:23 +0900

    Initial Commit

---
+++ Apptest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apptest
+//
+//  Created by 藤田優作 on 2020/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
